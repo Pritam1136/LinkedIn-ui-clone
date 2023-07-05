4a275f656d0f3499f6162d37fefeda82dce4094f
Pritam Roy <pritamroy1136@gmail.com>
2023-07-05 14:19:13 +0530

    New component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 /* eslint-disable no-unused-vars */
 import "./App.css";
 import Header from "./components/Header";
+import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
     <div className="app">
       {/* Header */}
       <Header />
+
       {/* App body */}
+      <div className="app_body">
+        {/* sidebar */}
+        <Sidebar />
 
-      {/* sidebar */}
+        {/* feed */}
 
-      {/* feed */}
-
-      {/* widges */}
+        {/* widges */}
+      </div>
     </div>
   );
 }
