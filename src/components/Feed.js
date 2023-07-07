d0feabefc6c887fe7c4af14b04ff34ca9e8eea71
Pritam Roy <pritamroy1136@gmail.com>
2023-07-07 09:56:35 +0530

    New component

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,10 +1,24 @@
+/* eslint-disable array-callback-return */
 /* eslint-disable no-unused-vars */
-import React from "react";
+import React, { useState } from "react";
 import "./Feed.css";
-import { Create } from "@mui/icons-material";
+import {
+  CalendarViewDay,
+  Create,
+  EventNote,
+  Image,
+  Subscriptions,
+} from "@mui/icons-material";
 import InputOption from "../inputOptions/InputOption";
+import Post from "../feedPost/Post";
 
 function Feed() {
+  const [posts, setPosts] = useState([]);
+
+  const sendPost = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <div className="feed">
       <div className="feed_inputContainer">
@@ -12,13 +26,28 @@ function Feed() {
           <Create />
           <form>
             <input type="text" />
-            <button type="submit">Send</button>
+            <button onClick={sendPost} type="submit">
+              Send
+            </button>
           </form>
         </div>
-        <div className="feed_inputOPtions">
-          {/* <InputOption Icon={} title="Photo" color="violet" /> */}
+        <div className="feed_inputOptions">
+          <InputOption Icon={Image} title="Photo" color="#70B5F9" />
+          <InputOption Icon={Subscriptions} title="Video" color="#E7A33E" />
+          <InputOption Icon={EventNote} title="Event" color="#C0CBCD" />
+          <InputOption Icon={CalendarViewDay} title="Article" color="#7FC15E" />
         </div>
       </div>
+
+      {/* Post */}
+      {posts.map((post) => {
+        <Post />;
+      })}
+      <Post
+        name="Pritam Roy"
+        description="This is a test"
+        message="WOW this worked"
+      />
     </div>
   );
 }
